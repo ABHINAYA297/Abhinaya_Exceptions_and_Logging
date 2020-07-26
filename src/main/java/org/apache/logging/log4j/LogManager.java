ac9ce2309da3c1974a7795ac1b226ef4f0b1a0dc
ABHINAYA297 <abhinayarani225@gmail.com>
2020-07-26 12:29:07 +0530

    Exceptions and Logging

---
+++ src/main/java/org/apache/logging/log4j/LogManager.java
@@ -0,0 +1,14 @@
+package org.apache.logging.log4j;
+
+import org.examplee.App;
+
+public class LogManager {
+
+	public static Logger getLogger(Class<App> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
---
+++ src/main/java/org/apache/logging/log4j/Logger.java
@@ -0,0 +1,37 @@
+package org.apache.logging.log4j;
+
+public class Logger {
+
+	public void debug(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void warn(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void error(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void fatal(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(double calculate_SI) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
